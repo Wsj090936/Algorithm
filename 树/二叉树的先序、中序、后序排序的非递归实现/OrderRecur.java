523e39e192d3a282e4d5e2b2535076df7527cbb0
wushijia <whalewsj@gmail.com>
2017-11-02 20:54:02 +0800

    2017/11/2

--- 树/二叉树的先序、中序、后序排序的非递归实现/OrderRecur.java
+++ 树/二叉树的先序、中序、后序排序的非递归实现/OrderRecur.java
@@ -19,4 +19,39 @@ public class OrderRecur {
 			}
 		}
 	}
+	public void inOrderRecur(Node2 root){//中序遍历
+		if(root != null){
+			Stack<Node2> stack = new Stack<>();
+			while(!stack.isEmpty() || root != null){
+				if(root != null){
+					stack.push(root);
+					root = root.left;
+				} else {
+					root = stack.pop();
+					System.out.println(root.value);
+					root = root.right;
+				}
+			}
+		}
+	}
+	public void posOrderRecur(Node2 root){//后序遍历
+		if(root != null){
+			Stack<Node2> stack1 = new Stack<>();
+			Stack<Node2> stack2 = new Stack<>();
+			stack1.push(root);
+			while(!stack1.isEmpty()){
+				root = stack1.pop();
+				stack2.push(root);//每次将stack1中的数据弹出，压入栈中，然后将stack2中的数据全部弹出，最后得到的就是后序遍历的顺序
+				if(root.left != null){
+					stack1.push(root.left);
+				}
+				if(root.right != null){
+					stack1.push(root.right);
+				}
+			}
+			while(!stack2.isEmpty()){
+				System.out.println(stack2.pop().value);
+			}
+		}
+	}
 }
